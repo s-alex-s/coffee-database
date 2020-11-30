78a1a2688431f64d5c0aaa9fae444a6d97ee6512
Alex <s-alex-smir@yandex.ru>
2020-11-30 15:46:14 +0600

    add new window add and edit functions in development

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QPushButton
+from PyQt5.QtCore import pyqtSignal
 
 
 class Window(QMainWindow):
@@ -10,6 +11,8 @@ class Window(QMainWindow):
         uic.loadUi('main.ui', self)
         self.textEdit.setReadOnly(True)
         self.pushButton.clicked.connect(self.show_coffee)
+        self.pushButton_2.clicked.connect(self.edit_window)
+        self.pushButton_3.clicked.connect(self.add_window)
 
     def show_coffee(self):
         con = sqlite3.connect('coffee.sqlite')
@@ -18,6 +21,7 @@ class Window(QMainWindow):
         self.textEdit.clear()
         self.textEdit.setText(self.textEdit.toPlainText() + '---------------------------------\n')
         for i in coffee_list:
+            self.textEdit.setText(self.textEdit.toPlainText() + 'id: ' + str(i[0]) + '\n')
             self.textEdit.setText(self.textEdit.toPlainText() + 'Сорт: ' + i[1] + '\n')
             self.textEdit.setText(self.textEdit.toPlainText() + 'Степень обжарки: ' + i[2] + '\n')
             self.textEdit.setText(self.textEdit.toPlainText() + 'Молотый/в зернах: ' + i[3] + '\n')
@@ -26,6 +30,36 @@ class Window(QMainWindow):
             self.textEdit.setText(self.textEdit.toPlainText() + 'Объем упаковки: ' + str(i[6]) + ' г.\n')
             self.textEdit.setText(self.textEdit.toPlainText() + '---------------------------------\n')
         con.close()
+
+    def edit_window(self):
+        global ex2
+        ex2 = SecondWindow()
+        ex2.show()
+        ex2.edit_coffee()
+
+    def add_window(self):
+        global ex2
+        ex2 = SecondWindow()
+        ex2.show()
+        ex2.add_coffee()
+
+
+class SecondWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.save_coffee)
+
+    def add_coffee(self):
+        self.label_7.hide()
+        self.spinBox_3.hide()
+        self.setWindowTitle('Добавить кофе')
+
+    def edit_coffee(self):
+        self.setWindowTitle('Редактировать кофе')
+
+    def save_coffee(self):
+        pass
 
 
 if __name__ == '__main__':
